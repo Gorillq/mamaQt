7de97831108ade6b229af65df5122a217b8d325a
Gorillq <kamil.kownacki3@gmail.com>
2024-02-25 00:21:28 +0100

    :rocket: Deploy stuff

--- main.cpp
+++ main.cpp
@@ -26,18 +26,24 @@ void dialog() {
     QDialog * dialog = new QDialog;
     QFormLayout * forms = new QFormLayout;
     QList<QLineEdit *> fields;
+    QList<QLineEdit *> wagaf;
     dialog->setLayout(forms);
-    forms->addRow(new QLabel("Wpisz artykuły"));
+    forms->addRow(new QLabel("Wpisz produkt\t\tWaga(gram)"));
     for (int i = 0; i < 4; ++i) {
         QLineEdit *lineEdit = new QLineEdit(dialog);
+        lineEdit->setAlignment(Qt::AlignRight);
+        QLineEdit *waga = new QLineEdit(dialog);
+        waga->setAlignment(Qt::AlignRight);
         QString label = QString("Artykuł %1").arg(i + 1);
-        forms->addRow(label, lineEdit);
-
+        QString labelon = QString("Waga(g) %1").arg(i + 1);
+        forms->addRow(lineEdit, waga);
         fields << lineEdit;
+        wagaf << waga;
     }
 // Add some standard buttons at the bottom
     QDialogButtonBox buttonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel,
                                Qt::Horizontal, dialog);
+    forms->addRow(new QLabel("\t1 kilogram to 1000 gram"));
     forms->addRow(&buttonBox);
     QObject::connect(&buttonBox, SIGNAL(accepted()), dialog, SLOT(accept()));
     QObject::connect(&buttonBox, SIGNAL(rejected()), dialog, SLOT(reject()));
@@ -45,36 +51,48 @@ void dialog() {
 // Show the dialog as modal
     if (dialog->exec() == QDialog::Accepted) {
         QVector<QString> objekt;
+        QVector<double> vwaga;
         QVector<double> objekty;
         // If the user didn't dismiss the dialog, do something with the fields
 //                foreach(QLineEdit *lineEdit, fields) {
         for (int i = 0; i < fields.size(); ++i) {
             objekt.push_back(fields[i]->text());
+            double tempn = wagaf[i]->text().toDouble();
+            vwaga.push_back(tempn * 0.01f);
         }
         for (int i = 0; i < objekt.size(); ++i) {
             QString tex = objekt[i];
             if (mama.contains(tex)) {
-                objekty.append(mama.value(tex));
+                objekty.append(mama.value(tex) * vwaga[i]);
             }
         }
         QMessageBox *boxxy = new QMessageBox;
         QString gogogo = QString::number(std::accumulate(objekty.begin(), objekty.end(), 0.0));
+        //switch statement bugs out (?):  then brute
         if (objekty.size() == 1) {
             QString nogo = objekt[0];
-            boxxy->setText("Znaleziono: 1 \n" + nogo + "\n" + gogogo + " kcal");
+            QString nnogo = QString::number(vwaga[0] * 0.1f);
+            boxxy->setText("Znaleziono: 1 \n" + nogo + " : " + nnogo + " kg\nWynik: " + gogogo + " kcal");
             boxxy->exec();
         }
         if (objekty.size() == 2) {
             QString nogo = objekt[0];
             QString nogo2 = objekt[1];
-            boxxy->setText("Znaleziono: 2 \n" + nogo + " " + nogo2 + "\n" + gogogo + " kcal");
+            QString nnogo = QString::number(vwaga[0] * 0.1f);
+            QString nnogo2 = QString::number(vwaga[1] * 0.1f);
+            boxxy->setText("Znaleziono: 2 \n" + nogo + " : " + nnogo + " kg\n" + nogo2
+                + " : " + nnogo2 + " kg\nWynik: " + gogogo + " kcal");
             boxxy->exec();
         }
         if (objekty.size() == 3) {
             QString nogo = objekt[0];
             QString nogo2 = objekt[1];
             QString nogo3 = objekt[2];
-            boxxy->setText("Znaleziono: 3 \n" + nogo + " " + nogo2 + " " + nogo3 + "\n" + gogogo + " kcal");
+            QString nnogo = QString::number(vwaga[0] * 0.1f);
+            QString nnogo2 = QString::number(vwaga[1] * 0.1f);
+            QString nnogo3 = QString::number(vwaga[2] * 0.1f);
+            boxxy->setText("Znaleziono: 3 \n" + nogo + " : " + nnogo + " kg\n" + nogo2
+                           + " : " + nnogo2 + "kg\n" + nogo3 + " : " + nnogo3 + "kg\nWynik:  " + gogogo + " kcal");
             boxxy->exec();
         }
         if (objekty.size() == 4) {
@@ -82,7 +100,13 @@ void dialog() {
             QString nogo2 = objekt[1];
             QString nogo3 = objekt[2];
             QString nogo4 = objekt[3];
-            boxxy->setText("Znaleziono: 4 \n" + nogo + " " + nogo2 + " " + nogo3 + " " + nogo4 + "\n" + gogogo + " kcal");
+            QString nnogo = QString::number(vwaga[0] * 0.1f);
+            QString nnogo2 = QString::number(vwaga[1] * 0.1f);
+            QString nnogo3 = QString::number(vwaga[2] * 0.1f);
+            QString nnogo4 = QString::number(vwaga[3] * 0.1f);
+            boxxy->setText("Znaleziono: 3 \n" + nogo + " : " + nnogo + " kg\n" + nogo2
+                           + " : " + nnogo2 + "kg\n" + nogo3 + " : " + nnogo3 + "kg\n " + nogo4
+                           + " : " + nnogo4 + " kg\nWynik: " + gogogo + " kcal");
             boxxy->exec();
         }
         }
